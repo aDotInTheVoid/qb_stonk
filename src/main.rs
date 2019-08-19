18f38cd89e1de8150fb425ef4c7153aeef1ddd11
Nixon <nixon.emoony@gmail.com>
2019-08-19 09:21:28 -0400

    basics

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use serde::{Serialize, Deserialize};
+use std::collections::HashMap;
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Portfolio{
+    shares: HashMap<String, u64>,
+    dollars: f64,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct MarketSnapshot{
+    traders: HashMap<String, Portfolio>,
+    prices: HashMap<String, f64>,
+}
+
+fn get_prices_url_interactive(){
+    do {
+        let mut url = String::new();
+    }    
+}
+
 fn main() {
    println!("Hello, world!");
 }
